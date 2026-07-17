38a5dbc5b4b90b6243576bd1b3b5aeebcb6b6535
Z-Ready <zrd1316176591@gmail.com>
2026-07-17 19:41:54 +1000

    Remove deprecated files and update .gitignore for new data structure

--- tests/test_data_loader.py
+++
@@ -1,6 +0,0 @@
-from src.data_loader import load_data
-
-def test_load_data():
-    data = load_data('data/train/data.csv')
-    assert data is not None
-    assert len(data) > 0
--- tests/test_model.py
+++
@@ -1,8 +0,0 @@
-from src.model import SimpleModel
-import torch
-
-def test_model():
-    model = SimpleModel()
-    input_data = torch.randn(1, 10)
-    output = model(input_data)
-    assert output.shape == (1, 2)
